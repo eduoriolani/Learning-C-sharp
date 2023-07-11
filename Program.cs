d52a7345aa1a0b8976c509a9b4d425dda222ae5c
Eduardo Oriolani <lemarch@MacBook-Pro-de-Eduardo.local>
2023-07-10 22:05:29 -0300

    ejercicios 1 y 2 openBootcamp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola mancos, al lobby");
+﻿//Ejercicio 1
+Console.WriteLine("Ingrese su nombre");
+string nombre = Console.ReadLine();
+Console.WriteLine("El nombre ingresado es " + nombre);
+
+string miNombre = "Eduardo Oriolani";
+Console.WriteLine("Mi nombre es " + miNombre);
+//Perdón no sabía si debía escribir mi nombre en consola o simplemente declarar la variable e imprimirla
+
+
+//Ejercicio 2
+DateTime horaActual = DateTime.Now;
+
+Console.WriteLine("La hora actual es " + horaActual);
