14f465df42e7ed6fa64d960af7fde62466a84a30
Eduardo Oriolani <lemarch@MacBook-Pro-de-Eduardo.local>
2023-07-13 07:02:29 -0300

    Sesion 2 ejercicios C# terminados

--- Program.cs
+++ Program.cs
@@ -1,14 +1,63 @@
-﻿//Ejercicio 1
-Console.WriteLine("Ingrese su nombre");
-string nombre = Console.ReadLine();
-Console.WriteLine("El nombre ingresado es " + nombre);
+﻿
+/////////////// SESION 1 //////////////////
+// //Ejercicio 1
+// Console.WriteLine("Lo que a vos te divierte a mi me asusta");
+// string nombre = Console.ReadLine();
+// Console.WriteLine("El nombre ingresado es " + nombre);
 
-string miNombre = "Eduardo Oriolani";
-Console.WriteLine("Mi nombre es " + miNombre);
-//Perdón no sabía si debía escribir mi nombre en consola o simplemente declarar la variable e imprimirla
+// string miNombre = "Eduardo Oriolani";
+// Console.WriteLine("Mi nombre es " + miNombre);
+// //Perdón no sabía si debía escribir mi nombre en consola o simplemente declarar la variable e imprimirla
 
 
-//Ejercicio 2
-DateTime horaActual = DateTime.Now;
+// //Ejercicio 2
+// DateTime horaActual = DateTime.Now;
 
-Console.WriteLine("La hora actual es " + horaActual);
+// Console.WriteLine("La hora actual es " + horaActual);
+
+/////////////////// SESION 2 //////////////////////
+
+//Ejercicio 1
+
+string nombre = "Carlos";
+string apellido = "Molineri";
+short edad = 25;
+string mensaje = "sabe de C#";
+
+Console.WriteLine("El es " + nombre + " " + apellido + " su edad es de: " + edad + " años, y ademas " + mensaje);
+ 
+
+ //Ejercicio 2
+
+ //Coche
+ byte puertas = 4;
+ byte ruedas = 4;   
+ string marca = "Honda";
+ int itvVigente = 2025; //En argentina solo se indica la fecha de caducidad
+Console.WriteLine("El coche tiene " + puertas + " puertas, " + ruedas + " ruedas, la marca del mismo es " + marca + " y su itv está vigente hasta " + itvVigente);
+
+ //Mesa
+ byte peso = 30;
+ float largo = 1.20f;
+ string material = "algarrobo";
+ string color = "marrón";
+Console.WriteLine("La mesa tiene un peso de " + peso + "kg, un largo de " + largo + "mts, es de " + material + " y su color es " + color);
+
+ //Ejercicio 3
+
+char caracter = 'a';
+bool comparacion = caracter == 'a';
+Console.WriteLine(comparacion);
+
+bool condicion1 = true;
+bool condicion2 = true;
+bool resultado = condicion1 && condicion2;
+Console.WriteLine(resultado);
+
+bool condicion3 = true;
+bool condicion4 = false;
+bool resultado2 = condicion3 || condicion4;
+Console.WriteLine(resultado2);
+
+
+ 
